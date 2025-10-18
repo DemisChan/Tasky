c2fcda6ab06ef6434a3824e012430890cba98fff
Demis <demilavr@gmail.com>
2025-10-18 09:00:55 +0100

    refactor(auth): Relocate LoginScreen and related files
    - Move `LoginScreen` and associated components into a new `login` sub-package within `feature.auth.presentation`.
    - Remove Timber initialization from `MainActivity` as it is now handled in the `TaskyApplication` class.

--- app/src/main/java/com/dmd/tasky/MainActivity.kt
+++ app/src/main/java/com/dmd/tasky/MainActivity.kt
@@ -8,18 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.dmd.tasky.feature.auth.presentation.TaskyLoginScreen
+import com.dmd.tasky.feature.auth.presentation.login.TaskyLoginScreen
 import com.dmd.tasky.ui.theme.TaskyTheme
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
         enableEdgeToEdge()
         setContent {
             TaskyTheme {
